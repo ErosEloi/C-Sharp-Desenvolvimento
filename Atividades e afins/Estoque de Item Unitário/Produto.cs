fe8af61ea057e497f136ab66262e04aadf13100b
Eros <80933549+ErosEloi@users.noreply.github.com>
2023-03-25 19:43:33 -0300

    Add files via upload

---
+++ Atividades e afins/Estoque de Item Unitário/Produto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace EstoqueUnitario
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Valor;
+        public int Quantidade;
+
+        public double ValorTotal()
+        {
+            return Valor * Quantidade;
+        }
+
+        public double Adicionar(int botar)
+        {
+            return Quantidade += botar;
+        }
+
+        public double Retirar(int tirar)
+        {
+            return Quantidade -= tirar;
+        }
+
+        public override string ToString()
+        {
+            return "Produto: " +Nome +" Valor: " +Valor +" Quantidade: " +Quantidade +" Valor total: "+ValorTotal().ToString();
+        }
+    }
+
+}
