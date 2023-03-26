52c8cbbdc426ab2d088ba767720ddd8fde211ed0
Eros <80933549+ErosEloi@users.noreply.github.com>
2023-03-25 23:14:04 -0300

    Add files via upload

---
+++ Atividades e afins/Pseud. Conta Bancaria/Cliente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancaria
+{
+    internal class Cliente
+    {
+
+        public int NumeroConta;
+        public double Saldo;
+
+        public Cliente(int numeroConta)
+        {
+            NumeroConta = numeroConta;
+        }
+
+        public double Adicionar(double botar)
+        {
+            return Saldo += botar;
+        }
+
+        public double Retirar(double tirar)
+        {
+            return Saldo -= tirar;
+        }
+
+        
+
+
+    }
+}
