73cc00370d64fbb25a8342b636ef04c94cd7532a
Eros <80933549+ErosEloi@users.noreply.github.com>
2023-04-05 16:29:19 -0300

    Add files via upload

---
+++ Atividades e afins/Ordem de Pedidos/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdemPedidos.Entities.Enums
+{
+    internal enum OrderStatus : int
+    {
+
+        Pending_Payment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Deliverd = 3
+
+    }
+}
